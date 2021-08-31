30b2190b08005c613091c394ea9feaad6779050d
unknown <rapozao2007@gmail.com>
2021-08-31 13:06:03 -0300

    atualizaa

--- js/style.js
+++ js/style.js
@@ -1,47 +1,71 @@
-document.querySelector('.section-8 .perguntas .title .icon-1').addEventListener('click', () => {
+document.querySelector('.section-8 .perguntas .title1').addEventListener('click', () => {
     if (document.querySelector('.p1').style.display == 'block') {
         document.querySelector('.p1').style.display = 'none'
+        document.querySelector('.icon-1').classList.remove('fa-angle-down')
+        document.querySelector('.icon-1').classList.add('fa-angle-right')
     } else {
         document.querySelector('.p1').style.display = 'block'
+        document.querySelector('.icon-1').classList.remove('fa-angle-right')
+        document.querySelector('.icon-1').classList.add('fa-angle-down')
     }
 })
 
-document.querySelector('.section-8 .perguntas .title .icon-2').addEventListener('click', () => {
+document.querySelector('.section-8 .perguntas .title2').addEventListener('click', () => {
     if (document.querySelector('.p2').style.display == 'block') {
         document.querySelector('.p2').style.display = 'none'
+        document.querySelector('.icon-2').classList.remove('fa-angle-down')
+        document.querySelector('.icon-2').classList.add('fa-angle-right')
     } else {
         document.querySelector('.p2').style.display = 'block'
+        document.querySelector('.icon-2').classList.remove('fa-angle-right')
+        document.querySelector('.icon-2').classList.add('fa-angle-down')
     }
 })
 
-document.querySelector('.section-8 .perguntas .title .icon-3').addEventListener('click', () => {
+document.querySelector('.section-8 .perguntas .title3').addEventListener('click', () => {
     if (document.querySelector('.p3').style.display == 'block') {
         document.querySelector('.p3').style.display = 'none'
+        document.querySelector('.icon-3').classList.remove('fa-angle-down')
+        document.querySelector('.icon-3').classList.add('fa-angle-right')
     } else {
         document.querySelector('.p3').style.display = 'block'
+        document.querySelector('.icon-3').classList.remove('fa-angle-right')
+        document.querySelector('.icon-3').classList.add('fa-angle-down')
     }
 })
 
-document.querySelector('.section-8 .perguntas .title .icon-4').addEventListener('click', () => {
+document.querySelector('.section-8 .perguntas .title4').addEventListener('click', () => {
     if (document.querySelector('.p4').style.display == 'block') {
         document.querySelector('.p4').style.display = 'none'
+        document.querySelector('.icon-4').classList.remove('fa-angle-down')
+        document.querySelector('.icon-4').classList.add('fa-angle-right')
     } else {
         document.querySelector('.p4').style.display = 'block'
+        document.querySelector('.icon-4').classList.remove('fa-angle-right')
+        document.querySelector('.icon-4').classList.add('fa-angle-down')
     }
 })
 
-document.querySelector('.section-8 .perguntas .title .icon-5').addEventListener('click', () => {
+document.querySelector('.section-8 .perguntas .title5').addEventListener('click', () => {
     if (document.querySelector('.p5').style.display == 'block') {
         document.querySelector('.p5').style.display = 'none'
+        document.querySelector('.icon-5').classList.remove('fa-angle-down')
+        document.querySelector('.icon-5').classList.add('fa-angle-right')
     } else {
         document.querySelector('.p5').style.display = 'block'
+        document.querySelector('.icon-5').classList.remove('fa-angle-right')
+        document.querySelector('.icon-5').classList.add('fa-angle-down')
     }
 })
 
-document.querySelector('.section-8 .perguntas .title .icon-6').addEventListener('click', () => {
+document.querySelector('.section-8 .perguntas .title6').addEventListener('click', () => {
     if (document.querySelector('.p6').style.display == 'block') {
         document.querySelector('.p6').style.display = 'none'
+        document.querySelector('.icon-6').classList.remove('fa-angle-down')
+        document.querySelector('.icon-6').classList.add('fa-angle-right')
     } else {
         document.querySelector('.p6').style.display = 'block'
+        document.querySelector('.icon-6').classList.remove('fa-angle-right')
+        document.querySelector('.icon-6').classList.add('fa-angle-down')
     }
 })
